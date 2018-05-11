387d62db5026ed50bb72fe787628c49e29bb5d18
lsj <877942720@qq.com>
2018-05-11 10:23:12 +0800

    5-5

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,7 +1,15 @@
 
 from rest_framework import serializers
+from goods.models import Goods,GoodsCategory
 
-class GoodsSerializer(serializers.Serializer):
-    name = serializers.CharField(required=True,max_length=100)
-    click_num = serializers.IntegerField(default=0)
-    goods_front_image = serializers.ImageField()
+class CategorySerialize(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsCategory
+        fields = "__all__"
+
+class GoodsSerializer(serializers.ModelSerializer):
+    category = CategorySerialize()
+    class Meta:
+        model = Goods
+        fields = "__all__"
+        # fields = ('name', 'click_num', 'market_price', 'add_time')
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,6 +1,8 @@
 from .serializers import GoodsSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
 
 from .models import Goods
 
@@ -12,3 +14,10 @@ class GoodsListView(APIView):
         snippets = Goods.objects.all()
         goods_serializer = GoodsSerializer(snippets, many=True)
         return Response(goods_serializer.data)
+
+    # def post(self, request, format=None):
+    #     serializer = GoodsSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
